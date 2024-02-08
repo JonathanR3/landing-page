8e8791cfd622b47a7bdcbda89682c743596d17b5
Jonathan <jonathan.rong@stonybrook.edu>
2024-02-08 07:02:18 -0500

    updated projects

--- javascript.js
+++ javascript.js
@@ -1,1 +1,68 @@
-console.log("Testing");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const container = document.querySelector('.aboutme');
+const description = document.createElement('p');
+description.setAttribute('style', 'color: red; font-size: 30px');
+description.classList.add('description');
+description.textContent = "I'm Red!";
+
+const hd3 = document.createElement('h3');
+hd3.setAttribute('style', 'color: blue; font-size: 50px');
+hd3.classList.add('headings');
+hd3.textContent = "I'm Blue!";
+
+const divider = document.createElement('div');
+divider.setAttribute('style', 'border-color: white; background-color:pink; border-width: 30px');
+divider.classList.add('borders');
+
+const txt = document.createElement('h1');
+txt.classList.add('placetxt');
+txt.textContent = "I'm in a div";
+
+const par = document.createElement('p');
+par.classList.add('placepar');
+par.textContent = "ME TOO";
+
+container.appendChild(description);
+container.appendChild(hd3);
+divider.appendChild(txt);
+divider.appendChild(par);
+container.appendChild(divider);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+})
+
+const buttons = document.querySelector(html);
+
+
+<div id = "cont">
+    <button id = "btn1">CLICK</button>
+    <button id = "btn2">CLICK</button>
+    <button id = "btn3">CLICK</button>
+</div>
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.id);
+    });
+});
+*/
+
